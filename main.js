96e9ec47ec3c977eb9deafc51f229c6fb6f51616
Dmytro Minchenko <mail@wer1k.pro>
2016-10-16 16:44:07 +0300

    Фінальаня версія, причесана та перевірена.

--- main.js
+++ main.js
@@ -2,59 +2,85 @@
  * Created by Dmytro on 13.10.2016.
  */
 
-'use strict';
 
-let rectangle1 = { // Координати першого прямокутника.
-    x1:40,
-    y1:80,
-    x2:90,
-    y2:60,
-};
+const rectangle1 = getRandomRectangle(),
+      rectangle2 = getRandomRectangle();
 
-let rectangle2 = { // Координати другого прямокутника.
-    x1:60,
-    y1:70,
-    x2:110,
-    y2:30,
-};
-
-let r1 = { // Визначаємо сторони першого прямокутника.
-    left: Math.min(rectangle1.x1, rectangle1.x2),
-    right: Math.max(rectangle1.x1, rectangle1.x2),
-    top: Math.max(rectangle1.y1, rectangle1.y2),
-    bottom: Math.min(rectangle1.y1, rectangle1.y2),
-};
-
-let r2 = { // Визначаємо сторони другого прямокутника.
-    left: Math.min(rectangle2.x1, rectangle2.x2),
-    right: Math.max(rectangle2.x1, rectangle2.x2),
-    top: Math.max(rectangle2.y1, rectangle2.y2),
-    bottom: Math.min(rectangle2.y1, rectangle2.y2),
-};
-
-function isIntersect() { // Перевіряємо на НЕ перетин прямокутників.
-    if (r1.left > r2.right || r1.right < r2.left || r1.top < r2.bottom || r1.bottom > r2.top) { // Перевірка на НЕ перетин прямокутників.
-        return false; // Якщо НЕ перетинається, повертаємо false.
-    } else {
-        return true; // Якщо перетинається, повертаємо true.
-    }
-}
-
-if (isIntersect() == true) {
+if (isIntersect(rectangle1, rectangle2)) {
     alert('Прямокутники перетинаються!');
-    var rectangle3 = {
+
+    const rectangle3 = {
         x1: Math.max(rectangle1.x1, rectangle2.x1),
         y1: Math.min(rectangle1.y1, rectangle2.y1),
         x2: Math.min(rectangle1.x2, rectangle2.x2),
         y2: Math.max(rectangle1.y2, rectangle2.y2),
     };
+
+    const ellipse = {
+        x : (rectangle3.x2 - rectangle3.x1) / 2 + rectangle3.x1,
+        y : (rectangle3.y2 - rectangle3.y1) / 2 + rectangle3.y1,
+        rx: Math.abs((rectangle3.x2 - rectangle3.x1) / 2),
+        ry: Math.abs((rectangle3.y2 - rectangle3.y1) / 2),
+    };
+
+    let array = [];
+
+    while (array.length < 10) {
+        const point                = getRandomPoint(),
+              ifPointInsideEllipse = checkIfPointInsideEllipse(point, ellipse);
+
+        if (ifPointInsideEllipse ) {
+            array.push(point)
+        }
+    }
+
+    document.write(JSON.stringify(array));
+
 } else {
-    alert('Прямокутники не перетинаються!');
+    alert('Прямокутники не пересікаються!');
 }
 
-let elipse = {
-    x0: (rectangle3.x2 - rectangle3.x1) /2 + rectangle3.x1,
-    y0: (rectangle3.y2 - rectangle3.y1) /2 + rectangle3.y1,
-    r1: Math.abs((rectangle3.x2 - rectangle3.x1) /2),
-    r2: Math.abs((rectangle3.y2 - rectangle3.y1) /2),
-};
+function checkIfPointInsideEllipse (point, ellipse) {
+    const partX = Math.pow((point.x - ellipse.x), 2) / Math.pow(ellipse.rx, 2),
+          partY = Math.pow((point.y - ellipse.y), 2) / Math.pow(ellipse.ry, 2);
+
+    return partX + partY <= 1;
+}
+
+function getRandomPoint () {
+    return {
+        x: getRandomCoordinate(),
+        y: getRandomCoordinate(),
+    };
+}
+
+function getRandomCoordinate () {
+    return Math.round(Math.random() * 100);
+}
+
+function getRandomRectangle () {
+    const x1 = getRandomCoordinate(),
+          y1 = getRandomCoordinate(),
+          x2 = getRandomCoordinate(),
+          y2 = getRandomCoordinate();
+
+    return {
+          x1    : x1,
+          y1    : y1,
+          x2    : x2,
+          y2    : y2,
+          left  : Math.min(x1, x2),
+          right : Math.max(x1, x2),
+          top   : Math.max(y1, y2),
+          bottom: Math.min(y1, y2),
+    }
+}
+
+function isIntersect(rectangle1, rectangle2) { // Проверка на пересечение (нам нужно чтоб пересечение было)
+    return !(
+           rectangle1.left   > rectangle2.right
+        || rectangle1.right  < rectangle2.left
+        || rectangle1.top    < rectangle2.bottom
+        || rectangle1.bottom > rectangle2.top
+    );
+}
