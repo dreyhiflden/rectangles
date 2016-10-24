3efca83ec7d6a8ad8b455d2acab8154528fcfbb5
Dmytro Minchenko <mail@wer1k.pro>
2016-10-24 20:20:25 +0300

    Change isIntersect function

--- main.js
+++ main.js
@@ -100,11 +100,11 @@ function getRandomRectangle() {
     }
 }
 
-function isIntersect(rectangle1, rectangle2) {
+function isIntersect (rectangle1, rectangle2) {
     return (
-            (rectangle1.left >= rectangle2.left) && (rectangle1.left <= rectangle2.right)
-        )
-        || (
-            (rectangle2.bottom >= rectangle1.bottom) && (rectangle2.bottom <= rectangle1.top)
-        )
+         !((rectangle1.left >= rectangle2.right)
+        || (rectangle1.right <= rectangle2.left)
+        || (rectangle1.top <= rectangle2.bottom)
+        || (rectangle1.bottom >= rectangle2.top))
+    )
 }
